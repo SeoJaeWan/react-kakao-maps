c1eb0aa8a5db10210c1044e2770baa1cbfc5d153
SeoJaeWan <sjw7324@naver.com>
2021-02-23 12:46:20 +0900

    modify

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,26 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es6': true,
+  env: {
+    browser: true,
+    es6: true,
   },
-  'extends': [
-    'google',
-    'plugin:react/recommended'
-  ],
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly',
+  extends: ["google", "plugin:react/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 2018,
-    'sourceType': 'module',
+    ecmaVersion: 2018,
+    sourceType: "module",
   },
-  'plugins': [
-    'react',
-    'react-hooks',
-  ],
-  'rules': {
-    'react-hooks/rules-of-hooks': 'error',
-    'require-jsdoc': 0,
-    'valid-jsdoc': 0,
+  plugins: ["react", "react-hooks"],
+  rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "require-jsdoc": 0,
+    "valid-jsdoc": 0,
+    quotes: ["off", "single"],
+    "object-curly-spacing": [6, "always"],
   },
 };
